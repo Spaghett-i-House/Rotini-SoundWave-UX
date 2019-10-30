c774e50bf115372708b666e52d9ff3b1c38a349c
morrison-ben <morrisonb84@gmail.com>
2019-10-30 19:29:35 -0400

    updated main html page, app module

--- RotiniSoundwaveUX/src/app/app.module.ts
+++ RotiniSoundwaveUX/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ColorPickerModule } from './color-picker/color-picker.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorPickerModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
